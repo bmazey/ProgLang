ac3f0c9f98e1b96d34b20e8dd92734f2b268929b
Brandon <b.mazey@nyu.edu>
2019-02-21 16:45:46 -0500

    added solution to #1, also multi-line comment

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,21 @@ module Main where
 import Lib
 
 main :: IO ()
-main = putStrLn "new dawn, new day!" -- this is a comment!
+
+{-
+  multi-line comment!
+-}
+
+-- main = putStrLn "new dawn, new day!"
+main = putStrLn(simpleReverse ['1', '2', '3', '4'])
+
+
+-- returns the reverse of a simple list
+simpleReverse :: [a] -> [a]
+simpleReverse [] = []
+simpleReverse(x:xs) = simpleReverse xs ++ [x]
+
+-- union two lists into a set
+unionSet :: [a] -> [a] -> [a]
+unionSet [] = []
+unionSet(x:xs) = if [x]
