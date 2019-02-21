9e49107a9e064c3a1f5ad94c11a4f7981cdb1426
Brandon <b.mazey@nyu.edu>
2019-02-21 18:01:05 -0500

    ok looking good!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import Data.List
 
 main :: IO ()
 
@@ -9,7 +10,10 @@ main :: IO ()
 -}
 
 -- main = putStrLn "new dawn, new day!"
-main = putStrLn(simpleReverse ['1', '2', '3', '4'])
+main = do
+    print $ simpleReverse [1, 2, 3, 4]
+    print $ listUnion [1, 2, 3] [2, 3, 4]
+    print $ insertionSort [4, 5, 1, 0, 8, 7, 3]
 
 
 -- returns the reverse of a simple list
@@ -17,7 +21,17 @@ simpleReverse :: [a] -> [a]
 simpleReverse [] = []
 simpleReverse(x:xs) = simpleReverse xs ++ [x]
 
--- union two lists into a set
-unionSet :: [a] -> [a] -> [a]
-unionSet [] = []
-unionSet(x:xs) = if [x]
+-- union two lists into a set (remove duplicates)
+listUnion :: Eq a => [a] -> [a] -> [a]
+listUnion x y = nub (x ++ y)
+
+-- sort list in ascending order
+place :: Int -> [Int] -> [Int]
+place x [] = [x]
+place x (y:ys) = if x < y
+                  then x:y:ys
+                  else y : place x ys
+
+insertionSort :: [Int] -> [Int]
+insertionSort [x] = [x]
+insertionSort (x:xs) = place x (insertionSort xs)
