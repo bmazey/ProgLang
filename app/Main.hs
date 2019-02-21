64a232da4c993059aa3db3e3f708e7bcc9012533
Brandon <b.mazey@nyu.edu>
2019-02-21 18:30:17 -0500

    ready to submit ^_^

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,16 @@ import Data.List
 main :: IO ()
 
 {-
-  multi-line comment!
+  this is a multi-line comment!
 -}
 
--- main = putStrLn "new dawn, new day!"
 main = do
     print $ simpleReverse [1, 2, 3, 4]
     print $ listUnion [1, 2, 3] [2, 3, 4]
     print $ insertionSort [4, 5, 1, 0, 8, 7, 3]
+    print $ minMax [11, 12, 13, 14]
+    print $ permute ['a', 'b', 'c']
+    print $ permute [1, 2, 3]
 
 
 -- returns the reverse of a simple list
@@ -21,7 +23,7 @@ simpleReverse :: [a] -> [a]
 simpleReverse [] = []
 simpleReverse(x:xs) = simpleReverse xs ++ [x]
 
--- union two lists into a set (remove duplicates)
+-- union two lists into a set (remove duplicates, different notation)
 listUnion :: Eq a => [a] -> [a] -> [a]
 listUnion x y = nub (x ++ y)
 
@@ -33,5 +35,19 @@ place x (y:ys) = if x < y
                  else y : place x ys
 
 insertionSort :: [Int] -> [Int]
+insertionSort [] = []
 insertionSort [x] = [x]
 insertionSort (x:xs) = place x (insertionSort xs)
+
+-- return largest and smallest numbers in list
+minMax :: [Int] -> [Int]
+minMax [] = []
+minMax (x:xs) = [minimum xs, maximum xs]
+
+-- return all permutations of a list
+permute :: Eq a => [a] -> [[a]]
+permute [] = [[]]
+permute xs = do x <- xs
+                let l = delete x xs
+                ls <- permute l
+                return $ x : ls
